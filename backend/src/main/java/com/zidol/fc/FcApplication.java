d9bcb47a1f2bedae22f87d6ccf0375aeae5cc965
Jinho KIM <88222461+jinokiim@users.noreply.github.com>
2022-05-27 21:36:20 +0900

    Add files via upload

---
+++ backend/src/main/java/com/zidol/fc/FcApplication.java
@@ -0,0 +1,13 @@
+package com.zidol.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcApplication.class, args);
+	}
+
+}
