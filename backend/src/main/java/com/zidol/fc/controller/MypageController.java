8623c57b940a6d1d5b3cc9a435d6a45ae14b73ac
Almondshin <Almondshin8@gmail.com>
2022-05-30 23:23:48 +0900

    추가변경

---
+++ backend/src/main/java/com/zidol/fc/controller/MypageController.java
@@ -0,0 +1,20 @@
+package com.zidol.fc.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MypageController {
+	
+	
+	
+	@PostMapping("/userProfile")
+	public Map<String,String> test2(@RequestBody Map<String, String> param){
+	
+		
+		return null;
+	}
+}
--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -1,7 +1,23 @@
 function MyPage() {
+  const profiletest = () => {
+    fetch("/userProfile", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      // body: JSON.stringify({}),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("res : ", res);
+      });
+  };
+
   return (
     <div>
       <div>MyPage</div>
+      <button onClick={profiletest}>test</button>
     </div>
   );
 }
