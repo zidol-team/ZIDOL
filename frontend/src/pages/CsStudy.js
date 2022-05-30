f9708260f5c4791d66a15162118a79e52272d035
Jinho Kim <wlsghrla94@gmail.com>
2022-05-30 13:56:21 +0900

    연결확인

---
+++ frontend/src/pages/CsStudy.js
@@ -0,0 +1,32 @@
+const CsStudy = () => {
+  const handleSubmit = () => {
+    const requestOptions = {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        Accept: "application/json",
+        "Access-control-allow-origin": "http://112.172.225.17:8282",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify({
+        string: "helloworld",
+      }),
+    };
+    console.log("requestOptions : ", requestOptions);
+
+    fetch("http://112.172.225.17:8282/helloworld", requestOptions)
+      // .then((res) => res.json())
+      .then((res) => {
+        console.log("res : ", res);
+      });
+  };
+
+  return (
+    <div>
+      <div>CsStudy</div>
+      <button onClick={handleSubmit}>전송</button>
+    </div>
+  );
+};
+
+export default CsStudy;
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,93 +1,127 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+// import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
 
-function SignUp() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [answer, setAnswer] = useState("");
+const theme = createTheme();
 
-  const onEmailHandler = (event) => {
-    setEmail(event.currentTarget.value);
-  };
-  const onPasswordHandler = (event) => {
-    setPassword(event.currentTarget.value);
-  };
-  const onConfirmPasswordHandler = (event) => {
-    setConfirmPassword(event.currentTarget.value);
-  };
-  const onNicknameHandler = (event) => {
-    setNickname(event.currentTarget.value);
-  };
-  const onAnswerHandler = (event) => {
-    setAnswer(event.currentTarget.value);
-  };
-  const onSubmitHandler = (event) => {
+export default function SignUp() {
+  const handleSubmit = (event) => {
     event.preventDefault();
-
-    if (password !== confirmPassword) {
-      return alert("비밀번호와 비밀번호 확인은 같아야 합니다.");
-    }
-    // 전송버튼 누를때 데이터 전송 만들어야함.
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <Container>
-        <Row>
-          <Col>
-            <div>로고 넣어야함</div>
-          </Col>
-          <Col>
-            <form style={{ display: "flex", flexDirection: "column" }}>
-              <label>Email</label>
-              <input
-                type="email"
-                value={email}
-                onChange={onEmailHandler}
-              ></input>
-              <label>Password</label>
-              <input
-                type="password"
-                value={password}
-                onChange={onPasswordHandler}
-              ></input>
-              <label>Confirm Password</label>
-              <input
-                type="password"
-                value={confirmPassword}
-                onChange={onConfirmPasswordHandler}
-              ></input>
-              <label>Nickname</label>
-              <input
-                type="text"
-                value={nickname}
-                onChange={onNicknameHandler}
-              ></input>
-              <label>Answer</label>
-              <input
-                type="text"
-                value={answer}
-                onChange={onAnswerHandler}
-              ></input>
-              <br />
-              <button type="submit">가입하기</button>
-            </form>
-          </Col>
-        </Row>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
       </Container>
-    </div>
+    </ThemeProvider>
   );
 }
-
-export default SignUp;
