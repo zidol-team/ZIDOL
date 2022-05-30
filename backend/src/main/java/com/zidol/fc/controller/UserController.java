4e265e5a86792596d2d8e487e1ebdbf3094075ba
woo <dndlsgur77@gmail.com>
2022-05-30 14:12:51 +0900

    cors 해결

--- backend/src/main/java/com/zidol/fc/controller/UserController.java
+++ backend/src/main/java/com/zidol/fc/controller/UserController.java
@@ -2,10 +2,12 @@ package com.zidol.fc.controller;
 
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class UserController {
 	
