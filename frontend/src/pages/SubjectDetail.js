97cc463596aed8578d723b0cfcb4e7d70d00f996
Almondshin <Almondshin8@gmail.com>
2022-06-07 23:13:01 +0900

    어드민페이지 및 서브젝트 디테일 내용 추가

--- frontend/src/pages/SubjectDetail.js
+++ frontend/src/pages/SubjectDetail.js
@@ -4,23 +4,11 @@ import { useLocation, useNavigate, useParams } from "react-router-dom";
 const SubjectDetail = ({}) => {
   const navigate = useNavigate();
   const location = useLocation();
-  // console.log(location.state);
-  // console.log(location.state.csType);
-  // console.log(location.state.studyData);
-  // console.log(location.state.studyData[csType]);
-  // console.log(location.state.studyData[csName]);
   /* 
   버튼 클릭했을때 선택된 과목 = location.state.csType
   전체 공부 데이터 = location.state.studyData
   */
   const studyData = location.state.studyData;
-  // const [studyIndex, setStudyIndex] = useState([]);
-  // const [selected, setSelected] = useState("");
-
-  useEffect(() => {
-    //
-  }, []);
-
   // 선택한 과목과 데이터상의 과목이름이 같을때
   const mapStudyData = studyData.map((a, index) => {
     if (location.state.csType === a.csType) {
@@ -51,8 +39,6 @@ const SubjectDetail = ({}) => {
           {a.csName}
         </div>
       );
-      // setStudyIndex([...studyData, a.csName]);
-      // console.log(studyIndex);
     }
   });
 
