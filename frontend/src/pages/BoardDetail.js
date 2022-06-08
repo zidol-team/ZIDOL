59efc8c39a627733dbab7dbdf4822df31e9b63de
pipi <pkh9847@gmail.com>
2022-06-08 11:10:44 +0900

    board

--- frontend/src/pages/BoardDetail.js
+++ frontend/src/pages/BoardDetail.js
@@ -1,6 +1,6 @@
 import React, { Link, useLocation, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import "../components/NoticeDetail.css";
+
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ReactHtmlParser from "react-html-parser";
@@ -24,15 +24,14 @@ const BoardDetail = ({}) => {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.data);
-        console.log(data.data.reply);
+        console.log(data);
+
         setBoard(data.data);
         setList1(data.data.reply);
       });
   }, [list2]);
 
   const deleteBoard = () => {
-    console.log(changelocationCode);
     fetch("/delete-board.act", {
       method: "DELETE",
       headers: {
@@ -48,7 +47,6 @@ const BoardDetail = ({}) => {
     });
   };
   const deleteReply = (replyCode) => {
-    console.log(replyCode);
     fetch("/delete-reply.act", {
       method: "DELETE",
       headers: {
@@ -84,7 +82,6 @@ const BoardDetail = ({}) => {
     })
       .then((res) => res.json())
       .then((res) => {
-        console.log("res : ", res);
         setReply("");
         setList2(list1);
         alert("댓글등록완료");
@@ -92,94 +89,92 @@ const BoardDetail = ({}) => {
   };
   const changeReply = (e) => {
     setReply(e.target.value);
-    console.log(reply);
   };
 
   return (
     <>
-      <div className="container">
-        <table className="table">
-          <tr>
-            <td>제목</td>
-            <td>{board.boardTitle}</td>
-          </tr>
-          <tr>
-            <td>작성일</td>
-            <td>{board.boardRegDate}</td>
-          </tr>
-          <tr>
-            <td>작성자</td>
-            <td>{userName}</td>
-          </tr>
-        </table>
-        <div className="content">
-          <label>{ReactHtmlParser(board.boardContent)}</label>
-        </div>
-
-        <div className="buttons">
-          <Button
-            style={{ margin: "10px" }}
-            variant="outlined"
-            onClick={() =>
-              navigate(`/BoardModify`, {
-                state: {
-                  boardTitle: board.boardTitle,
-                  boardContent: board.boardContent,
-                  boardCode: board.boardCode,
-                },
-              })
-            }
-          >
-            수정
-          </Button>
-          <Button
-            style={{ margin: "10px" }}
-            variant="outlined"
-            startIcon={<DeleteIcon />}
-            onClick={() => deleteBoard()}
-          >
-            삭제
-          </Button>
-          <Button variant="outlined" onClick={() => navigate(`/Board`)}>
-            목록
-          </Button>
-        </div>
-        <textarea
-          value={reply}
-          onChange={changeReply}
-          className="replycontent"
-          name="message"
-          placeholder="Message"
-          rows="10"
-        ></textarea>
-        <div className="buttons">
-          <Button variant="outlined" onClick={submitReply}>
-            댓글등록
-          </Button>
-        </div>
-        <div>
-          {list1.map((a, index) => (
-            <div key={index}>
-              <div className="form-control">
-                <div>
-                  <span style={{ float: "left", marginRight: "10px" }}>
-                    {userName}
-                  </span>
-                  <div style={{ float: "right" }}>
-                    <Button onClick={() => deleteReply(a.replyCode)}>
-                      삭제
-                    </Button>
+      <div style={{ width: "100%" }}>
+        <div className="bcontainer">
+          <div style={{ margin: "10px", fontSize: "30px", fontWeight: "bold" }}>
+            Q.{board.boardTitle}
+          </div>
+          <div>
+            <span style={{ textAlign: "left", marginRight: "10px" }}>
+              {userName}
+            </span>
+            <span style={{ textAlign: "right" }}>{board.boardRegDate}</span>
+          </div>
+          <hr></hr>
+          <div className="content">
+            <label>{ReactHtmlParser(board.boardContent)}</label>
+          </div>
+          <div className="buttons">
+            <Button variant="outlined" onClick={() => navigate(`/Board`)}>
+              목록
+            </Button>
+            <Button
+              style={{ margin: "10px", float: "right" }}
+              variant="outlined"
+              onClick={() =>
+                navigate(`/BoardModify`, {
+                  state: {
+                    boardTitle: board.boardTitle,
+                    boardContent: board.boardContent,
+                    boardCode: board.boardCode,
+                  },
+                })
+              }
+            >
+              수정
+            </Button>
+            <Button
+              style={{ margin: "10px", float: "right" }}
+              variant="outlined"
+              startIcon={<DeleteIcon />}
+              onClick={() => deleteBoard()}
+            >
+              삭제
+            </Button>
+          </div>
+          <div className="areacontainer">
+            <textarea
+              value={reply}
+              onChange={changeReply}
+              className="replycontent"
+              name="message"
+              placeholder="Message"
+              rows="10"
+            ></textarea>
+          </div>
+          <div className="buttonss">
+            <Button variant="outlined" onClick={submitReply}>
+              댓글등록
+            </Button>
+          </div>
+          <div>
+            {list1.map((a, index) => (
+              <div key={index}>
+                <div className="form-control">
+                  <div>
+                    <span style={{ float: "left", marginRight: "10px" }}>
+                      {userName}
+                    </span>
+                    <div style={{ float: "right" }}>
+                      <Button onClick={() => deleteReply(a.replyCode)}>
+                        삭제
+                      </Button>
+                    </div>
+                    <span style={{ float: "right" }}>{a.replyRegdate}</span>
                   </div>
-                  <span style={{ float: "right" }}>{a.replyRegdate}</span>
-                </div>
-                <div>
-                  <span style={{ float: "inline-start" }}>
-                    {a.replyContent}
-                  </span>
+                  <div>
+                    <span style={{ float: "inline-start" }}>
+                      {a.replyContent}
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </>
