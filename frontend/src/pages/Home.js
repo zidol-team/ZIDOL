6826a8243422b44534b4de58721ae72b26d800f4
Jinho Kim <wlsghrla94@gmail.com>
2022-09-03 00:49:16 +0900

    css: mui 적용

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,28 +1,53 @@
-import { Container, Row, Col } from "react-bootstrap";
-import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
-import "./Home.css";
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import './Home.css';
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 // 확인
 function Home() {
+  const navigate = useNavigate();
+
+  const SingInButton = () => {
+    return (
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate('/SignIn');
+        }}
+      >
+        <Typography>로그인</Typography>
+      </Button>
+    );
+  };
+
+  const SingUpButton = () => {
+    return (
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate('/SignIn');
+        }}
+      >
+        <Typography>회원가입</Typography>
+      </Button>
+    );
+  };
+
   return (
-    <div className="mainContainer">
-      <div className="mainDesc">
-        <div className="homeTitle">Sea s</div>
-        <div className="mainTitle">CS공부 한 잔 하고 가세요.</div>
-      </div>
-      <div className="loginContainer">
-        <div>
-          <Link to="/SignIn" className="loginText">
-            로그인하기
-          </Link>
-        </div>
-        <div>
-          <Link to="/SignUp" className="loginText">
-            회원가입하기
-          </Link>
-        </div>
-      </div>
-    </div>
+    <Container className="mainContainer">
+      <Box className="mainDesc">
+        <Typography className="homeTitle">Sea s</Typography>
+        <Typography className="mainTitle">CS공부 한 잔 하고 가세요.</Typography>
+      </Box>
+      <Stack direction="row" spacing={2} sx={{ mx: 5, justifyContent: 'space-between' }}>
+        <SingInButton></SingInButton>
+        <SingUpButton></SingUpButton>
+      </Stack>
+    </Container>
   );
 }
 
